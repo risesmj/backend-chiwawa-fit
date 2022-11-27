438522c660a5957a56b0e68fc0ffd0baf0063938
MJ <matheusjcunha@gmail.com>
2022-11-27 17:36:38 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' })
   const config = new DocumentBuilder()
     .setTitle('ChiwawaFit API')
     .setDescription('API for application web and mobile of the ChiwawaFit')
